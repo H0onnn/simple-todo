c7b636c45e225f4ad1fb699e3d3e263252226b19
H0onnn <dukei2017@naver.com>
2025-02-15 11:15:22 +0900

    feat : kanban-title 컴포넌트 추가

---
+++ app/(pages)/_components/kanban/title.tsx
@@ -0,0 +1,33 @@
+import { useKanbanStore } from "@/app/shared/store/kanban";
+
+import { AddButton, FormDialog } from "@/app/(pages)/_components";
+import { overlay } from "@/app/shared/lib";
+
+export const Title = () => {
+  const { createColumn } = useKanbanStore();
+
+  const handleAddColumn = () => {
+    overlay.open(
+      <FormDialog
+        type="column"
+        mode="create"
+        onSubmit={(data) => {
+          createColumn({
+            title: data.title,
+            description: data.description,
+          });
+        }}
+      />
+    );
+  };
+
+  return (
+    <div className="my-3 flex items-center justify-between">
+      <span className="text-sm md:text-xl">
+        Manage your schedule simply and conveniently
+      </span>
+
+      <AddButton onClick={handleAddColumn} isIconOnly />
+    </div>
+  );
+};
